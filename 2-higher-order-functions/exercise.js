6634fc1d8146084dd2790db33b42cce045b70947
Shadab Ali <shadabali604@gmail.com>
2020-10-13 08:41:13 +0530

    added:

--- 2-higher-order-functions/exercise.js
+++ 2-higher-order-functions/exercise.js
@@ -107,8 +107,9 @@ function objOfMatches(array1, array2, callback) {
 
 //Extension 6
 function multiMap(arrVals, arrCallbacks) {
-
+    return arrVals.reduce((acc, cv) => {
+        acc[cv] = arrCallbacks.map(fn => fn(cv))
+    }, {})
 }
-
 // console.log(multiMap(['catfood', 'glue', 'beer'], [function(str) { return str.toUpperCase(); }, function(str) { return str[0].toUpperCase() + str.slice(1).toLowerCase(); }, function(str) { return str + str; }]));
 // should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }
